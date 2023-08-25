122ac184c2c4dbda391a2e16a513774d0efbc25c
Pinkcolorrr <54249627+Pinkcolorrr@users.noreply.github.com>
2023-08-26 01:42:43 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-k3pftu
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-k3pftu)
---
+++ index.js
@@ -0,0 +1,136 @@
+// Import stylesheets
+import './style.css';
+
+// Write Javascript code!
+const appDiv = document.getElementById('app');
+appDiv.innerHTML = `<h1>JS Starter</h1>`;
+
+class LinkedListNode {
+  value;
+  next;
+
+  constructor(value, next = null) {
+    this.value = value;
+    this.next = next;
+  }
+}
+
+class LinkedList {
+  head;
+  tail;
+
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
+
+  prepend(value) {
+    const newNode = new LinkedListNode(value, this.head);
+    this.head = newNode;
+
+    if (!this.tail) {
+      this.tail = this.head;
+    }
+  }
+
+  append(value) {
+    const newNode = new LinkedListNode(value);
+
+    if (!this.head && !this.tail) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+  }
+
+  delete(value) {
+    if (!this.head) {
+      return null;
+    }
+
+    let deletedNode = null;
+
+    while (this.head && this.head.value === value) {
+      deletedNode = this.head;
+      this.head = this.head.next;
+    }
+
+    let currentNode = this.head;
+    while (currentNode && currentNode.next) {
+      if (currentNode.next.value === value) {
+        deletedNode = currentNode.next;
+        currentNode.next = deletedNode.next;
+      } else {
+        currentNode = currentNode.next;
+      }
+    }
+
+    if (this.tail.value === value) {
+      this.tail = currentNode;
+    }
+
+    return deletedNode;
+  }
+
+  find(value) {
+    if (!this.head) {
+      return null;
+    }
+
+    let currentNode = this.head;
+    while (currentNode) {
+      if (currentNode.value === value) {
+        return currentNode;
+      }
+
+      currentNode = currentNode.next;
+    }
+
+    return null;
+  }
+
+  deleteTail() {
+    if (!this.tail) {
+      return null;
+    }
+
+    let deletedTail = this.tail;
+
+    if (this.tail === this.head) {
+      this.tail = null;
+      this.head = null;
+      return deletedTail;
+    }
+
+    let currentNode = this.head;
+    while (currentNode && currentNode.next) {
+      if (!currentNode.next.next) {
+        currentNode.next = null;
+      } else {
+        currentNode = currentNode.next;
+      }
+    }
+
+    this.tail = currentNode;
+    return deletedTail;
+  }
+
+  deleteHead() {
+    if (!this.head) {
+      return null;
+    }
+
+    let deletedNode = this.head;
+
+    if (this.head === this.tail) {
+      this.head = null;
+      this.tail = null;
+      return deletedNode;
+    }
+
+    this.head = this.head.next;
+    return deletedNode;
+  }
+}
